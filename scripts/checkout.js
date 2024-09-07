8e601852bf6ea64b0ceed5fe5f842dfdbee7e721
motaamneh <mtaamneh292@gmail.com>
2024-09-07 22:44:42 +0300

    fix radio buttons

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
-
+import { formatCurrency } from "./utils/money.js";
 
 let cartSummaryHTML=``;
 cart.forEach((cartItem) => {
@@ -26,7 +26,7 @@ products.forEach((product)=>{
                   ${matchingItem.name}
                 </div>
                 <div class="product-price">
-                  ${(matchingItem.priceCents /100).toFixed(2)}
+                  ${formatCurrency(matchingItem.priceCents)}
                 </div>
                 <div class="product-quantity">
                   <span>
@@ -48,7 +48,7 @@ products.forEach((product)=>{
                 <div class="delivery-option">
                   <input type="radio" checked
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingItem.id}">
                   <div>
                     <div class="delivery-option-date">
                       Tuesday, June 21
@@ -61,7 +61,7 @@ products.forEach((product)=>{
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingItem.id}">
                   <div>
                     <div class="delivery-option-date">
                       Wednesday, June 15
@@ -74,7 +74,7 @@ products.forEach((product)=>{
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingItem.id}">
                   <div>
                     <div class="delivery-option-date">
                       Monday, June 13
