c11245d503f75e29db6452353cb40ca8800257d8
motaamneh <mtaamneh292@gmail.com>
2024-10-07 13:49:10 +0300

    class

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,5 @@
 import { renderOrderSummary } from "./orderSummary.js";
 import { renderPaymentSummary } from "./paymentSummary.js";
+import '../data/cart-class.js';
 renderOrderSummary();
 renderPaymentSummary();
