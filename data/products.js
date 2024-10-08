4496e7ab60f68394cdcc25c79cca50346e71f1b7
motaamneh <mtaamneh292@gmail.com>
2024-10-08 09:41:45 +0300

    ..

--- data/products.js
+++ data/products.js
@@ -35,22 +35,7 @@ class Product{
 
 }
 
-const product1 = new Product({
-  id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-  name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090,
-  keywords: [
-    "socks",
-    "sports",
-    "apparel"
-  ]
-});
-console.log(product1);
+
 
 
 export const products = [
@@ -716,4 +701,3 @@ export const products = [
   return new Product(productDetails);
 });
 
-console.log(products);
